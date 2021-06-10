dec939767280abd1becd4aeeab92fd0474fc7354
Pedro Serpa <pedroserpah@gmail.com>
2021-06-10 02:17:45 -0300

    Creating database connection

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+import { Connection, createConnection, getConnectionOptions } from 'typeorm'
+
+const database = async (): Promise<Connection> => {
+
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database: process.env.NODE_ENV === 'test' ? './src/database/database.test.sqlite' : defaultOptions.database
+        })
+    );
+}
+
+export default database;
